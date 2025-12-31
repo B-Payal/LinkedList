2a15365b1e7a87c145ff565b6d3a527412d09e40
Payal <b08.payal@gmail.com>
2025-12-31 11:45:01 +0530

    Add files via upload

---
+++ linkedlist.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+#reversing a linked list
+
+def Reverse(head):
+    curr=head
+    prev=None
+    while curr!=None:
+        next=curr.next
+        curr.next=prev
+        prev=curr
+        curr=next
+    return prev
+
+#check if the list a palindrome or not
+# def palindrome(head):
+#     val=[]
+#     curr=head
+#     while curr!=None:
+#         val.append(curr.data)
+#         curr=curr.next
+#     if val==val[::-1]:
+#         return True
+#     else:
+#         return False
+
+#searching for an element in a linked list
+def Search(head,key):
+    curr=head
+    while curr!=None:
+        if curr.data==key:
+            return True
+        curr=curr.next
+    return False    
+
+
+
+
+n = int(input())
+ll = list(map(int , input().split()))
+key = int(input())
+head = None
+tail = None
+for i in range(n):
+    node = Node(ll[i])
+    if head is None:
+        head = node
+        tail = node
+    else:
+        tail.next = node
+        tail = node    
+
+
+
+# head=Reverse(head)
+# temp=head
+# while temp!=None:
+#     print(temp.data,end=" ")
+#     temp=temp.next
+    
+print(Search(head,key))
